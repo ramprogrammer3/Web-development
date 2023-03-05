4207f15239dc4abe362d0c4107bc303cfeba282c
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2023-03-05 23:20:34 +0530

    javascript-1

---
+++ javaScript/index.js
@@ -0,0 +1,34 @@
+for (let i = 0; i <= 100; i++) {
+  console.log(i);
+}
+
+console.log("Hi ram kumar");
+
+let name = "ram kumar";
+console.log(typeof name);
+let a = 10;
+console.log(a);
+a = "ram";
+console.log(a);
+a = false;
+console.log(a);
+
+var age = 10;
+console.log(age);
+const number = 14;
+// number = 34;
+console.log(number);
+
+console.log(2 + 3)
+console.log(3 - 2)
+console.log(3**2);
+console.log(12 % 5)
+
+console.log(2>3)
+console.log(3>=3)
+console.log(5>3)
+console.log(5===5)
+console.log(5==5)
+console.log(5!=5)
+console.log(5!==5)
+
